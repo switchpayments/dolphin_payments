8831f69feeaa7d776feef8b95c1f9c9551586f24
Rui-Pedro <48125495+Rui-Pedro@users.noreply.github.com>
2022-01-13 13:58:44 +0100

    Add webhook handler

--- Dummie/routes/transactions.js
+++ Dummie/routes/transactions.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let XMLHttpRequest = require('xhr2');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+function webhookHandler(orderIdentifier, callbackUrl) {
+    setTimeout(function() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', callbackUrl, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            orderIdentifier: orderIdentifier,
+        }));
+      }, 5000);
+}
 
 module.exports = router;
