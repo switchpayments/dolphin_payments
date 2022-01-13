c9cb1f31bd6776f74f051ad046a2b6bd069a3032
Rui-Pedro <48125495+Rui-Pedro@users.noreply.github.com>
2022-01-13 14:07:41 +0100

    Add random int function

--- Dummie/routes/transactions.js
+++ Dummie/routes/transactions.js
@@ -7,15 +7,32 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/**
+ * Send the webhook to the callback URL with the status of the transaction
+ * 
+ * @param orderIdentifier - Order identifier
+ * @param callbackUrl - URL we want to send the webhook
+ */
 function webhookHandler(orderIdentifier, callbackUrl) {
-    setTimeout(function() {
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', callbackUrl, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-            orderIdentifier: orderIdentifier,
-        }));
-      }, 5000);
+  setTimeout(function() {
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', callbackUrl, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+          orderIdentifier: orderIdentifier,
+      }));
+    }, getRandomInt());
+}
+
+/**
+ * Get random integer between 2000 and 10000
+ * 
+ * @returns Random integer between 2000 and 10000
+ */
+function getRandomInt() {
+  min = Math.ceil(2000);
+  max = Math.floor(10000);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 module.exports = router;
